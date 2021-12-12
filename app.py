aaee056b2fa1b143ac57a2755e7096f012be26c2
RaffaeleMosca <raffaele.mosca001@studenti.uniparthenope.it>
2021-12-12 12:17:57 +0100

    inserimento commenti

--- app.py
+++ app.py
@@ -9,16 +9,16 @@ app = Flask(__name__)
 app.secret_key = b'\xcc^\x91\xea\x17-\xd0W\x03\xa7\xf8J0\xac8\xc5'
 
 # Database locale
-#client = pymongo.MongoClient('localhost', 27017)
-#db = client.login
+client = pymongo.MongoClient('localhost', 27017)
+db = client.login
 
 
 #Database su cloud
-app.config['MONGO_URI'] = 'mongodb+srv://RafMosca:RafMoscaDB@cluster0.tt9vd.mongodb.net/login?retryWrites=true&w=majority'
-mongo = PyMongo(app)
-db = mongo.db
+#app.config['MONGO_URI'] = 'mongodb+srv://RafMosca:RafMoscaDB@cluster0.tt9vd.mongodb.net/login?retryWrites=true&w=majority'
+#mongo = PyMongo(app)
+#db = mongo.db
 
-# Decorators
+# FUNZIONE CHE CONTROLLA SE SI E' LOGGATI
 def login_required(f):
   @wraps(f)
   def wrap(*args, **kwargs):
@@ -26,7 +26,7 @@ def login_required(f):
       return f(*args, **kwargs)
     else:
       return redirect('/')
-  
+
   return wrap
 
 # Routes
@@ -52,6 +52,32 @@ def raccolta():
 def utente():
     return render_template('page_account.html')
 
+@app.route('/foodbox/')
+@login_required
+def cibo():
+    foodb = db.foodbox.find()
+    return render_template('cibo.html', foodb=foodb)
+
+@app.route('/creabox/')
+@login_required
+def creabox():
+    return render_template('creabox.html')
+
+@app.route('/mybox/')
+@login_required
+def mybox():
+    ordinazione = db.ordinazioni.find()
+    return render_template('mybox.html', ordinazione=ordinazione)
+
+@app.route('/myboxcaricati/')
+@login_required
+def myboxcaricati():
+    foodb = list(db.foodbox.find())
+    ordi = list(db.ordinazioni.find())
+    return render_template('myboxcaricati.html', foodb=foodb, ordi=ordi)
+
+
+#FUNZIONE CHE MODIFICA LA PASSWORD
 @app.route('/modificapass/<utente>', methods=['GET', 'POST'])
 def modificapass(utente):
     utentee = db.users.find_one({'email': utente})
@@ -66,16 +92,10 @@ def modificapass(utente):
         flash('Password errata!')
         return render_template('page_account.html')
 
-@app.route('/foodbox/')
-@login_required
-def cibo():
-    foodb = db.foodbox.find()
-    return render_template('cibo.html', foodb=foodb)
-
-
-@app.route('/add', methods=['POST'])
-def add_todo():
-    rifiuto = request.form.get('new-todo')
+#FUNZIONE CHE DATO UN RIFIUTO IN INPUT RESTITUISCE DOVE SMALTIRLO
+@app.route('/ricerca', methods=['POST'])
+def ricerca():
+    rifiuto = request.form.get('rifiuto')
     if db.raccolta.find_one({'rifiuto': rifiuto}):
         trovato = db.raccolta.find_one({'rifiuto': rifiuto})
         dove = trovato['dove']
@@ -84,6 +104,7 @@ def add_todo():
         flash('Rifiuto non trovato!')
         return render_template('raccolta.html')
 
+#FUNZIONE PER ORDINARE UNA FOODBOX
 @app.route('/ordina/<oid>,<utente>', methods=('POST',))
 def ordina(oid, utente):
     box = db.foodbox.find_one({'_id': ObjectId(oid)})
@@ -99,20 +120,14 @@ def ordina(oid, utente):
 
     return redirect('/foodbox/')
 
+#FUNZIONE PER ELIMINARE UNA FOODBOX E LE RELATIVE ORDINAZIONE
 @app.route('/elimina/<oid>', methods=('POST',))
 def elimina(oid):
     db.foodbox.delete_one({"_id": ObjectId(oid)})
     db.ordinazioni.delete_many({"box_number": ObjectId(oid)})
     return redirect('/myboxcaricati/')
 
-
-
-
-@app.route('/creabox/')
-@login_required
-def creabox():
-    return render_template('creabox.html')
-
+#FUNZIONE PER CREARE UNA FOODBOX
 @app.route('/create/<utente>', methods=['GET', 'POST'])
 def create(utente):
     tipo = request.form.get('tipo')
@@ -123,21 +138,7 @@ def create(utente):
     db.foodbox.insert_one({'utente': utente, 'tipo': tipo, 'contenuto': contenuto, 'quantita': int(quantita), 'negozio': negozio, 'indirizzo': indirizzo})
     return render_template('creabox.html')
 
-
-
-@app.route('/mybox/')
-@login_required
-def mybox():
-    ordinazione = db.ordinazioni.find()
-    return render_template('mybox.html', ordinazione=ordinazione)
-
-@app.route('/myboxcaricati/')
-@login_required
-def myboxcaricati():
-    foodb = list(db.foodbox.find())
-    ordi = list(db.ordinazioni.find())
-    return render_template('myboxcaricati.html', foodb=foodb, ordi=ordi)
-
+#FUNZIONE PER ELIMINARE UN ORDINAZIONE
 @app.route('/eliminaordi/<oid>', methods=('POST',))
 def eliminaordi(oid):
     ordi = db.ordinazioni.find_one({'_id': ObjectId(oid)})
@@ -159,6 +160,7 @@ def eliminaordi(oid):
         db.ordinazioni.delete_one({"_id": ObjectId(oid)})
     return redirect('/mybox/')
 
+#FUNZIONE PER ELIMINARE L'ORDINAZIONE UNA VOLTA RITIRATO IL FOODBOX
 @app.route('/boxritirato/<oid>', methods=('POST',))
 def boxritirato(oid):
     db.ordinazioni.delete_one({"_id": ObjectId(oid)})
